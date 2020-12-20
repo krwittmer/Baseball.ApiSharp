4b59fe6f3a9acbd5ffbfca9d8cea21c96e6d3e53
Kevin Wittmer <krwittmer@outlook.com>
2020-12-20 00:38:06 -0600

    Added SQL index files under early dev

---
+++ Baseball.ApiSharp.Dal/ExtendedSchema/OreillyBaseballHacks/newhacks_create_indexes_only.sql
@@ -0,0 +1,10 @@
+﻿use lahmans2019db;
+
+create index batting_pidx on batting(playerID);
+create index people_idx_1 on people(playerID);
+create index people_idx_2 on people(managerID);
+create index pitching_idx_1 on pitching(yearID,teamID);
+create index fielding_idx_1 on fielding(yearID,teamID);
+create index fieldingof_idx_1 on fieldingof(playerID, yearID);
+create index salaries_idx_1 on salaries(playerID);
+create index salaries_idx_2 on salaries(yearID,teamID);
---
+++ Baseball.ApiSharp.Dal/ExtendedSchema/OreillyBaseballHacks/newhacks_drop_indexes_only.sql
@@ -0,0 +1,10 @@
+﻿use lahmans2019db;
+
+drop index batting_pidx on batting;
+drop index people_idx_1 on people;
+drop index people_idx_2 on people;
+drop index pitching_idx_1 on pitching;
+drop index fielding_idx_1 on fielding;
+drop index fieldingof_idx_1 on fieldingof;
+drop index salaries_idx_1 on salaries;
+drop index salaries_idx_2 on salaries;
