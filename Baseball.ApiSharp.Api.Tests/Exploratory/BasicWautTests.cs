3c76cd301d3b0441587643d2c5879815ed1504e6
Kevin Wittmer <krwittmer@outlook.com>
2022-08-10 21:19:18 -0500

    Added WebApp under tests

---
+++ Baseball.ApiSharp.Api.Tests/Exploratory/BasicWautTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+
+namespace Baseball.ApiSharp.Api.Tests.Exploratory
+{
+    public class BasicWautTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BasicWautTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task Get_EndpointsReturnSuccess(string url)
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
